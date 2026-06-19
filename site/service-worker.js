bc9fef9eb387673a61ace4816d44c62773104883
pingshen670924-dotcom <pingshen670924-dotcom@users.noreply.github.com>
2026-06-19 21:31:04 +0800

    Update free independent mobile 539 system

--- site/service-worker.js
+++ site/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="539-mobile-20260619062706";
+const CACHE="539-mobile-20260619213020";
 async function clearAllCaches(){
   const keys=await caches.keys();
   await Promise.all(keys.map(key=>caches.delete(key)));
