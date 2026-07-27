f95f43b0fb2e5390d16bb891c74155290a1b0cef
539 automation <actions@users.noreply.github.com>
2026-07-27 15:41:54 +0000

    Update 539 data and reports

--- site/service-worker.js
+++ site/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="539-mobile-20260727233332";
+const CACHE="539-mobile-20260727234153";
 async function clearAllCaches(){
   const keys=await caches.keys();
   await Promise.all(keys.map(key=>caches.delete(key)));
